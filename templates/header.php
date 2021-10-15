d04f2e0e0a3ccc2a6beaf17a13dc892ee0319266
MatheusSouza13 <matheus.souza107@fatec.sp.gov.br>
2021-10-14 23:08:42 -0300

    Banco de dados criado, arquivo de conexão e modelo inseridos na pasta config

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,6 @@
 <head>
     <title>Nosso Site de Ingressos</title>
-    <link rel="stylesheet" href="ttps://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" teste>
+    <link rel="stylesheet" href="ttps://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 </head>
 <body class="grey lighten-4">
     
